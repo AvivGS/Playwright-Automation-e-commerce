f56e8433c501965ef9dd5e74ec4077f78e636e55
Aviv Gavriel Shimony <aviv.g.sh@gmail.com>
2024-09-26 14:42:59 +0300

    Separate to test files

---
+++ tests/laptopCategory.spec.js
@@ -0,0 +1,54 @@
+import { test, chromium, expect } from "@playwright/test";
+import POManager from "../pages/POManager";
+
+const url = "https://www.demoblaze.com/";
+
+test("Check at least 1 laptop product is listed ", async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+
+  const poManager = new POManager(page);
+
+  const homePage = poManager.getHomePage();
+  const laptopsPage = poManager.getLaptopsPage();
+
+  /* 
+    // Set the cookie with the token
+    // await context.addCookies([
+    //   {
+    //     name: cookieTokenName,
+    //     value: cookieTokenValue,
+    //     domain: ".demoblaze.com",
+    //     path: "/",
+    //     httpOnly: false,
+    //     secure: true,
+    //   },
+    // ]);
+    */
+
+  await test.step("Navigate to home page", async () => {
+    await page.goto(url);
+    await page.waitForLoadState("load");
+  });
+
+  await test.step("Click on the laptops category", async () => {
+    await homePage.categoriesList.first().waitFor(); // Ensure categories are visible
+    await homePage.laptopsCategoryBtn.click();
+  });
+
+  await test.step("Check for available laptops", async () => {
+    await laptopsPage.laptopCard.first().waitFor(); // Ensure at least one laptop is visible
+    const laptopsCount = await laptopsPage.laptopCard.count(); // Get count of laptop cards
+    expect(laptopsCount).toBeGreaterThan(0); // Ensure there is at least one laptop
+
+    const laptopTitle = await laptopsPage.laptopTitle.first().textContent();
+    const laptopPrice = await laptopsPage.laptopPrice.first().textContent();
+
+    await expect(laptopsPage.laptopTitle.first()).toBeVisible(); // Ensure the title is visible
+    expect(laptopTitle).toBeTruthy();
+    expect(laptopPrice).toBeTruthy();
+  });
+
+  await browser.close();
+});
